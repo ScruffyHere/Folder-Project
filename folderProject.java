a5e9272e40990a4167644eb5a3beaf1b0d74150c
ScruffyHere <sinakarimi550@gmail.com>
2017-09-07 02:09:53 -0500

    changed thank you output

--- folderProject.java
+++ folderProject.java
@@ -7,11 +7,12 @@ public static void menu(){
     System.out.println("1. Fall");
     System.out.println("2. Spring");
     System.out.println("3. Summer");
-    System.out.println("Choose your semester's number: ");
+    System.out.println("\nChoose your semester's number: ");
 }
 
    public static void main(String[] args) {
      Scanner input = new Scanner(System.in);
+     
 
      System.out.print ("Type your folders base name: ");
         String mainFolder = input.nextLine();
@@ -26,7 +27,7 @@ public static void menu(){
         System.exit(0);
      }
 
-     System.out.print ("Type amount of subfolders needed within your base folder: ");
+     System.out.print ("\nType amount of subfolders needed within your base folder: ");
         int subFolder = input.nextInt();
 
      String[] sFolder = new String[subFolder];
@@ -44,27 +45,29 @@ public static void menu(){
             System.exit(0);
            }
 
-           do {
+         /*  do {
              menu();
-              int semesterNumber = input.nextInt();
+              int numSemester = input.nextInt();
 
-             switch( semesterNumber ){
-
+             switch(numSemester){
              case 1:
-                 
+                 System.out.println("Case 1");
+                 break;
 
              case 2:
-                 
+                 System.out.println("Case 2");
+                 break;
 
              case 3:
-                
+                 System.out.println("Case 3");
+                 break;
 
              default:
-                 System.out.println("Unknown selection.");
+                 System.out.println("unknown selection.");
                }
-           } while (semesterNumber < 1 || semesterNumber > 3);
+           } while(numSemester < 1 || numSemester > 3);*/
+       }
+         System.out.println("\nThank you for using my program.");
 
-      System.out.println("Thank you for using my program.");
-
-    }
+  }
 }
